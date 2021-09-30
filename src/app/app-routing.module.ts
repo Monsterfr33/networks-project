c618af09045ccd729d956734dfd00b57af0e2858
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2021-09-30 23:17:56 +0500

    fixes done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
+  },{
+    path: 'homepage',
+    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
   },
   {
     path: 'auth',
     loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule)
-  },
-  {
-    path: 'homepage',
-    loadChildren: () => import('./components/homepage/homepage.module').then(m => m.HomepageModule)
   },
   {
     path: 'profile',
